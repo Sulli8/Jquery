a7b71ea2acce39b2e9286de7ab41617f1eef6736
@Sulli8 <Sullivan.sextius@gmail.com>
2021-01-17 20:17:32 +0100

    + Possibilité de jouer deux coups avec l'algorithme

--- Morpion/scriptSolo.js
+++ Morpion/scriptSolo.js
@@ -3,68 +3,72 @@ $(function(){
   //on initialise le tableau qui permet le morpion
   var j = 0;
   var morpion = ["#1","#2","#3","#4","#5","#6","#7","#8","#9"];
-  j++;
+  var coteMorpion = ["#1","#3","#7","#9"];
+  var selecteurOrdi = "";
   for (var i = 0; i < morpion.length; i++) {
     //on appelle la fonction cliccase
-    j++;
     clicCase(morpion[i]);
   }
+  function patientez(){
+      $("#patientez").text("Patientez...").fadeOut(1000,function(){
+        $("#patientez").fadeIn(1000,function(){
+          $("#patientez").remove();
+        });
+      });
+  }
 
+  function entierAleatoire(min, max)
+  {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function borderclic(selecteur){
+    $(selecteur).css("border","1px solid red")
+
+  }
+  function tourOrdi(morpion,coteMorpion,selecteurOrdi,selecteur,j){
+    delete morpion[morpion.indexOf(selecteur)];
+    borderclic(selecteur);
+
+    for (var i = 0; i < coteMorpion.length; i++) {
+      if(selecteur == coteMorpion[i]){
+        coteMorpion.splice(coteMorpion.indexOf(selecteur),1);
+        selecteurOrdi = coteMorpion[entierAleatoire(0, coteMorpion.length - 1)];
+      }
+    }
+    setTimeout(function(){$(selecteurOrdi).html("<p>O</p>");},2000);
+    morpion.splice(morpion.indexOf(selecteurOrdi),1);
+    coteMorpion.splice(coteMorpion.indexOf(selecteurOrdi),1);
+  }
 function clicCase(selecteur){
   //on initialise le compteur de clic
 
   $(selecteur).click(function(){
-    j++;
     //on incrémente afin de compter le nombre de clic
-      $(selecteur).html("<p>X</p>");
+    j++;
+    $(selecteur).html("<p>X</p>");
+
+    tourOrdi(morpion,coteMorpion,selecteurOrdi,selecteur,j);
+    //incrémenter le tour de l'ordi
+    j++;
+    if(j%2 == 0){
       patientez();
-      setTimeout(function(){joueurOrdi(selecteur)},2000);
+    }
+    console.log(j);
+    console.log(coteMorpion);
+    console.log("#"+$(selecteur).attr('id'));
+    console.log(morpion);
+      //patientez();
+      //setTimeout(function(){joueurOrdi(selecteur)},2000);
+
   });
 };
 
-function joueurOrdi(selecteur){
-//  console.log("#"+$(selecteur).attr('id'));
-  var caseJoueur =  "#"+$(selecteur).attr('id');
-  var caseExterieur = ["#1","#3","#7","#9"];
-  var solutionOrdi = [];
-  for (var j = 0; j < caseExterieur.length; j++) {
-    if(caseJoueur ==  caseExterieur[j]){
-      for (var i = 0; i < caseExterieur.length; i++) {
-        if(caseJoueur != caseExterieur[i]){
-          solutionOrdi.push(caseExterieur[i]);
-        }
-      }
-      coupDulapin(solutionOrdi,caseExterieur,selecteur);
-    }
-  }
-}
-function patientez(){
-    $("#patientez").text("Patientez...").fadeOut(1000,function(){
-      $("#patientez").fadeIn(1000,function(){
-        $("#patientez").remove();
-      });
-    });
-}
 
-function entierAleatoire(min, max)
-{
- return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-//Utilisation
-//La variable contient un nombre aléatoire compris entre 1 et 10
-function coupDulapin(solutionOrdi,caseExterieur,selecteur){
-  var jeu = [];
-  var selecteurOrdi = solutionOrdi[entierAleatoire(0, solutionOrdi.length - 1)];
-  $(selecteurOrdi).html("<p>O</p>");
-  jeu.push(selecteurOrdi,selecteur);
-  var caseExterieurNonjouer = [];
-  for (var i = 0; i < caseExterieur.length; i++) {
-    if(caseExterieur[i] != jeu[0] && caseExterieur[i] !=  jeu[1]){
-      caseExterieurNonjouer.push(caseExterieur[i]);
-    }
-  }
 
-}
+
+
+
 
 
 
